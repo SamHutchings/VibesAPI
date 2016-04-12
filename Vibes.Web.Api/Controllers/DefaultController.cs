28883c1ddb59fcc95351fb5c81ac233025ad4e5b
Sam Hutchings <samhutchings@gmail.com>
2016-04-12 21:33:28 +0100

    let's use nhibernate!

--- Vibes.Web.Api/Controllers/DefaultController.cs
+++ Vibes.Web.Api/Controllers/DefaultController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Http;
 
 namespace Vibes.Web.Api.Controllers
 {
-	public class DefaultController : Controller
+	public class DefaultController : ApiController
 	{
 	}
 }
