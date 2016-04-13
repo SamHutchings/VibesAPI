2be3169732a1fb9682d21b6286f1948e20d5bd51
Sam Hutchings <samhutchings@gmail.com>
2016-04-13 20:13:53 +0100

    controllers. add api key for googl

---
+++ Vibes.Web.Api/Controllers/VibeController.cs
@@ -0,0 +1,19 @@
+﻿namespace Vibes.Web.Api.Controllers
+{
+	using System.Web.Http;
+	using Core.Domain;
+	using System;
+
+	public class VibeController : ApiController
+	{
+		public IHttpActionResult Get(string id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IHttpActionResult Post(Vibe vibe)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
