754aa8c55f019160aebdbcdfc8252c1f732a9d0a
Sam Hutchings <samhutchings@gmail.com>
2016-05-10 22:20:16 +0100

    csproj

--- Vibes.Web.Api/Controllers/ContactController.cs
+++ Vibes.Web.Api/Controllers/ContactController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace Vibes.Web.Api.Controllers
 {
-    public class ContactController : ApiController
+	public class ContactController : ApiController
     {
     }
 }
