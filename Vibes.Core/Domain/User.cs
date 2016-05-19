711e1b4265a9025761cefa93be2fc0919a5418f5
Sam Hutchings <samhutchings@gmail.com>
2016-05-19 22:00:09 +0100

    don't think device is needed - one device per user

--- Vibes.Core/Domain/User.cs
+++ Vibes.Core/Domain/User.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 namespace Vibes.Core.Domain
 {
 	public class User : DomainObject<int>
 	{
+		/// <summary>
+		/// The user's phone number. This is their unique identifier
+		/// </summary>
 		public string PhoneNumber { get; set; }
 
-		public ISet<Device> Devices { get; set; }
+		/// <summary>
+		/// The users name. Displayed to others
+		/// </summary>
+		public string Username { get; set; }
 
-		public User()
-		{
-			Devices = new HashSet<Device>();
-		}
+		/// <summary>
+		/// The current device id for the user
+		/// </summary>
+		public string DeviceId { get; set; }
+
+		/// <summary>
+		/// The validation key the user is sent to validate their phone number
+		/// </summary>
+		public string ValidationKey { get; set; }
+
+		/// <summary>
+		/// When the validation key was sent to the user
+		/// </summary>
+		public DateTime? ValidationKeySent { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime? Validated { get; set; }
 	}
 }
