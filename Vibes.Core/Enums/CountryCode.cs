d4b950bb3c38ba66ba3ed717c8753441acb50c95
Sam Hutchings <samhutchings@gmail.com>
2016-05-23 22:44:26 +0100

    finished

--- Vibes.Core/Enums/CountryCode.cs
+++ Vibes.Core/Enums/CountryCode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace Vibes.Core.Enums
 		Afghanistan = 93,
 		Albania = 355,
 		Algeria = 213,
+		[Description("American Samoa")]
 		AmericanSamoa = 1683,
 		Andorra = 376,
 		Angola = 244,
@@ -22,6 +24,7 @@ namespace Vibes.Core.Enums
 		Aruba = 297,
 		Ascension = 247,
 		Australia = 61,
+		[Description("Australian External Territories")]
 		AustralianExternalTerritories = 672,
 		Austria = 43,
 		Azerbaijan = 994,
@@ -37,60 +40,87 @@ namespace Vibes.Core.Enums
 		Bermuda = 1440,
 		Bhutan = 975,
 		Bolivia = 591,
+		[Description("Bosnia & Herzegovina")]
 		BosniaAndHerzegovina = 387,
 		Botswana = 267,
 		Brazil = 55,
+		[Description("British Virgin Islands")]
 		BritishVirginIslands = 1283,
+		[Description("Brunei Darussalam")]
 		BruneiDarussalam = 673,
 		Bulgaria = 359,
+		[Description("Burkina Faso")]
 		BurkinaFaso = 226,
 		Burundi = 257,
 		Cambodia = 855,
 		Cameroon = 237,
 		Canada = 1,
+		[Description("Cape Verde Islands")]
 		CapeVerdeIslands = 238,
+		[Description("Cayman Islands")]
 		CaymanIslands = 1344,
+		[Description("Central African Republic")]
 		CentralAfricanRepublic = 236,
 		Chad = 235,
+		[Description("Chatham Island")]
 		ChathamIsland = 64,
 		Chile = 56,
 		China = 86,
+		[Description("Christmas Island")]
 		ChristmasIsland = 53,
+		[Description("Cocos Islands")]
 		CocosKeelingIslands = 61,
 		Colombia = 57,
 		Comoros = 269,
 		Congo = 242,
+		[Description("Democratic Republic of Congo")]
 		DemRepOfCongo = 243,
+		[Description("Cook Islands")]
 		CookIslands = 682,
+		[Description("Costa Rica")]
 		CostaRica = 506,
+		[Description("Côte d'Ivoire")]
 		IvoryCoast = 225,
 		Croatia = 385,
 		Cuba = 53,
 		Curaçao = 599,
 		Cyprus = 357,
+		[Description("Czech Republic")]
 		CzechRepublic = 420,
 		Denmark = 45,
+		[Description("Diego Garcia")]
 		DiegoGarcia = 246,
 		Djibouti = 253,
 		Dominica = 1766,
+		[Description("Dominican Republic")]
 		DominicanRepublic = 1809,
+		[Description("East Timor")]
 		EastTimor = 670,
+		[Description("Easter Island")]
 		EasterIsland = 56,
 		Ecuador = 593,
 		Egypt = 20,
 		ElSalvador = 503,
+		[Description("Equatorial Guinea")]
 		EquatorialGuinea = 240,
 		Eritrea = 291,
 		Estonia = 372,
 		Ethiopia = 251,
+		[Description("Falkland Islands")]
 		FalklandIslands = 500,
+		[Description("Faroe Islands")]
 		FaroeIslands = 298,
+		[Description("Fiji Islands")]
 		FijiIslands = 679,
 		Finland = 358,
 		France = 33,
+		[Description("French Antilles")]
 		FrenchAntilles = 596,
+		[Description("French Guiana")]
 		FrenchGuiana = 594,
+		[Description("French Polynesia")]
 		FrenchPolynesia = 689,
+		[Description("Gabonese Republic")]
 		GaboneseRepublic = 241,
 		Gambia = 220,
 		Georgia = 995,
@@ -102,13 +132,14 @@ namespace Vibes.Core.Enums
 		Grenada = 1472,
 		Guadeloupe = 590,
 		Guam = 1670,
-		GuantanamoBay = 5399,
 		Guatemala = 502,
+		[Description("Guinea Bissau")]
 		GuineaBissau = 245,
 		Guinea = 224,
 		Guyana = 592,
 		Haiti = 509,
 		Honduras = 504,
+		[Description("Hong Kong")]
 		HongKong = 852,
 		Hungary = 36,
 		Iceland = 354,
@@ -125,9 +156,12 @@ namespace Vibes.Core.Enums
 		Kazakhstan = 7,
 		Kenya = 254,
 		Kiribati = 686,
+		[Description("North Korea")]
 		NorthKorea = 850,
+		[Description("South Korea")]
 		SouthKorea = 82,
 		Kuwait = 965,
+		[Description("Kyrgyz Republic")]
 		KyrgyzRepublic = 996,
 		Laos = 856,
 		Latvia = 371,
@@ -144,15 +178,19 @@ namespace Vibes.Core.Enums
 		Malawi = 265,
 		Malaysia = 60,
 		Maldives = 960,
+		[Description("Mali Republic")]
 		MaliRepublic = 223,
 		Malta = 356,
+		[Description("Marshall Islands")]
 		MarshallIslands = 692,
 		Martinique = 596,
 		Mauritania = 222,
 		Mauritius = 230,
+		[Description("Mayotte Island")]
 		MayotteIsland = 269,
 		Mexico = 52,
 		Micronesia = 691,
+		[Description("Midway Island")]
 		MidwayIsland = 1807,
 		Moldova = 373,
 		Monaco = 377,
@@ -166,21 +204,26 @@ namespace Vibes.Core.Enums
 		Nauru = 674,
 		Nepal = 977,
 		Netherlands = 31,
+		[Description("Netherlands Antilles")]
 		NetherlandsAntilles = 599,
 		Nevis = 1868,
+		[Description("New Caledonia")]
 		NewCaledonia = 687,
+		[Description("New Zealand")]
 		NewZealand = 64,
 		Nicaragua = 505,
 		Niger = 227,
 		Nigeria = 234,
 		Niue = 683,
+		[Description("Norfolk Island")]
 		NorfolkIsland = 672,
+		[Description("Northern Marianas Islands")]
 		NorthernMarianasIslands = 1669,
 		Norway = 47,
 		Oman = 968,
 		Pakistan = 92,
 		Palau = 680,
-		PalestinianSettlements = 970,
+		Palestine = 970,
 		Panama = 507,
 		PapuaNewGuinea = 675,
 		Paraguay = 595,
@@ -188,32 +231,45 @@ namespace Vibes.Core.Enums
 		Philippines = 63,
 		Poland = 48,
 		Portugal = 351,
-		PuertoRico = +1 - 787or+1-939,
+		PuertoRico = 1787,
 		Qatar = 974,
+		[Description("Réunion Island")]
 		RéunionIsland = 262,
 		Romania = 40,
 		Russia = 7,
-		RwandeseRepublic = 250,
+		Rwanda = 250,
+		[Description("St Helena")]
 		StHelena = 290,
+		[Description("St Kitts & Nevis")]
 		StKittsNevis = 1868,
+		[Description("St Lucia")]
 		StLucia = 1757,
+		[Description("St Pierre & Miquelon")]
 		StPierreMiquelon = 508,
+		[Description("St Vincent and the Grenadines")]
 		StVincentGrenadines = 1783,
 		Samoa = 685,
+		[Description("San Marino")]
 		SanMarino = 378,
-		SãoToméandPrincipe = 239,
+		[Description("Sao Tome and Principe")]
+		SaoTomeandPrincipe = 239,
+		[Description("Saudi Arabia")]
 		SaudiArabia = 966,
 		Senegal = 221,
 		Serbia = 381,
-		SeychellesRepublic = 248,
+		Seychelles = 248,
+		[Description("Sierra Leone")]
 		SierraLeone = 232,
 		Singapore = 65,
-		SlovakRepublic = 421,
+		Slovaki = 421,
 		Slovenia = 386,
+		[Description("Solomon Islands")]
 		SolomonIslands = 677,
-		SomaliDemocraticRepublic = 252,
+		Somalia = 252,
+		[Description("South Africa")]
 		SouthAfrica = 27,
 		Spain = 34,
+		[Description("Sri Lanka")]
 		SriLanka = 94,
 		Sudan = 249,
 		Suriname = 597,
@@ -225,29 +281,40 @@ namespace Vibes.Core.Enums
 		Tajikistan = 992,
 		Tanzania = 255,
 		Thailand = 66,
+		[Description("Timor-Leste")]
 		TimorLeste = 670,
+		[Description("Tologese Republic")]
 		TogoleseRepublic = 228,
 		Tokelau = 690,
-		TongaIslands = 676,
+		Tonga = 676,
+		[Description("Trinidad & Tobago")]
 		TrinidadTobago = 1867,
 		Tunisia = 216,
 		Turkey = 90,
 		Turkmenistan = 993,
+		[Description("Turks & Caicos Islands")]
 		TurksandCaicosIslands = 1648,
 		Tuvalu = 688,
 		Uganda = 256,
 		Ukraine = 380,
+		[Description("United Arab Emirates")]
 		UnitedArabEmirates = 971,
+		[Description("United Kingdom")]
 		UnitedKingdom = 44,
+		[Description("USA")]
 		UnitedStatesofAmerica = 1,
+		[Description("US Virgin Islands")]
 		USVirginIslands = 1339,
 		Uruguay = 598,
 		Uzbekistan = 998,
 		Vanuatu = 678,
+		[Description("Vatican City")]
 		VaticanCity = +39,
 		Venezuela = 58,
 		Vietnam = 84,
+		[Description("Wake Island")]
 		WakeIsland = 808,
+		[Description("Wallis & Futuna Islands")]
 		WallisandFutunaIslands = 681,
 		Yemen = 967,
 		Zambia = 260,
