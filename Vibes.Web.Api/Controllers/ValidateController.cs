3e7478dd2866fb63d36fe56557ce6f0053c7c678
Sam Hutchings <samhutchings@gmail.com>
2016-05-25 19:14:04 +0100

    validate not activate

---
+++ Vibes.Web.Api/Controllers/ValidateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vibes.Web.Api.Models;
+
+namespace Vibes.Web.Api.Controllers
+{
+	public class ValidateController : BaseApiController
+	{
+		public IHttpActionResult Post(ValidateModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				if (AuthorisedUser.ValidationCode != model.ValidationCode)
+				{
+
+				}
+			}
+
+			return BadRequest(ModelState);
+		}
+	}
+}
