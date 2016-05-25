ddc446221db81572552daff0bc8b6c5758d027e6
Sam Hutchings <samhutchings@gmail.com>
2016-05-25 22:33:52 +0100

    don't authorise for the time biengh

--- Vibes.Web.Api/Models/VibeModel.cs
+++ Vibes.Web.Api/Models/VibeModel.cs
@@ -1,13 +1,13 @@
 ﻿using System.Web.Http.ModelBinding;
 using Vibes.Core.Domain;
 using Vibes.Core.Enums;
+using Vibes.Web.Api.Infrastructure;
 
 namespace Vibes.Web.Api.Models
 {
 	public class VibeModel
 	{
-		[ModelBinder(typeof(UserModelBinder))]
-		public User To { get; set; }
+		public string To { get; set; }
 
 		public VibeType VibeType { get; set; }
 	}
