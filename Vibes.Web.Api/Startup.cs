8b6aee0a3f35667ccbdbe17a0e1d1d61c4c31161
Sam Hutchings <samhutchings@gmail.com>
2016-06-26 17:44:16 +0100

    put owin back - start of oauth

---
+++ Vibes.Web.Api/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Vibes.Web.Api.App_Start.Startup))]
+namespace Vibes.Web.Api
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			var config = new HttpConfiguration();
+			ConfigureAuth(app);
+			WebApiConfig.Register(config);
+			app.UseWebApi(config);
+		}
+	}
+}
